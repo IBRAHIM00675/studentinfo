b158908e69f59909ac86e433bc9fbc6747f09f76
ibrahim musa <miharbiibra675@gmail.com>
2024-10-04 00:36:17 +0300

    modified components & index.css

--- src/components/Create.jsx
+++ src/components/Create.jsx
@@ -1,46 +1,50 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
+
 const Create = () => {
 
 
 
-    return(
-        <Form >
-        <Form.Group className="mb-2" controlId="exampleForm.ControlInput1">
+    
+     
 
-          <Form.Label className=''>Name:</Form.Label>
-          <br/>
-          <Form.Control  type="text" name="Name"  placeholder="Enter Your Name" />
-          </Form.Group>
-            
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label className='text-sm'>Email:</Form.Label>
-          <br/>
-          <Form.Control  type="text" name="Email"  placeholder="Enter Your Email" />
-          </Form.Group>
 
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Course:</Form.Label>
-          <br/>
-          <Form.Control  type="text" name="Course"  placeholder="Enter Your Course" />
-          </Form.Group>
+  return (
+    <Form>
 
-          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Phone Number:</Form.Label>
-          <br/>
-          <Form.Control  type="tel" name="phone number"  placeholder="Enter Your phonenumber" />
-          </Form.Group>
-            
-       
-            
-         <Button id='save-btn' type='submit' variant="primary">save info</Button>
-        
-        
-      </Form>
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Name:</Form.Label>
+        <br/>
+        <Form.Control type="text" placeholder="Enter your name" />
+      </Form.Group>
       
-    );
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Email:</Form.Label>
+        <br/>
+        <Form.Control type="text" placeholder="name@example.com" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Phone Number:</Form.Label>
+        <br/>
+        <Form.Control type="tel" placeholder="Enter your number" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+        <Form.Label>Course:</Form.Label>
+        <br/>
+        <Form.Control type="text" placeholder="Enter your course" />
+      </Form.Group>
+
+     <Button variant="">Save info</Button>
+
+      
+    </Form>
+  );
 }
+
+
 
 
 
